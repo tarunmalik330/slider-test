131511dce3afc13a6d77fc6b6ba7f7be5de84571
tarunmalik330 <maliktarun2005@gmail.com>
2024-08-22 10:48:54 +0530

    form validation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Practice from "./components/practice";
 import TabsPractice from "./components/TabsPractice";
+import FormEmailJsPractice from "./components/FormEmailJsPractice";
 
 function App() {
   return (
     <div>
       <Practice />
       <TabsPractice />
+      <FormEmailJsPractice />
     </div>
   );
 }
---
+++ src/components/FormEmailJsPractice.jsx
@@ -0,0 +1,266 @@
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const FormEmailJsPractice = () => {
+  const form = useRef();
+
+  const [formValues, setFormValues] = useState({
+    user_name: "",
+    user_lastName: "",
+    user_number: "",
+    user_password: "",
+    user_confirmPassword: "",
+    user_email: "",
+    message: "",
+  });
+
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const validate = () => {
+    let errors = {};
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!formValues.user_name.trim()) {
+      errors.user_name = "Name is required";
+    }
+
+    if (!formValues.user_lastName.trim()) {
+      errors.user_lastName = "Last Name is required";
+    }
+
+    if (!formValues.user_number.trim()) {
+      errors.user_number = "Number is required";
+    } else if (!/^\d{10}$/.test(formValues.user_number)) {
+      errors.user_number = "Number must be 10 digits";
+    }
+
+    if (!formValues.user_password) {
+      errors.user_password = "Password is required";
+    } else if (formValues.user_password.length < 6) {
+      errors.user_password = "Password must be at least 6 characters long";
+    }
+
+    if (!formValues.user_confirmPassword) {
+      errors.user_confirmPassword = "Confirm Password is required";
+    } else if (formValues.user_password !== formValues.user_confirmPassword) {
+      errors.user_confirmPassword = "Passwords do not match";
+    }
+
+    if (!formValues.user_email.trim()) {
+      errors.user_email = "Email is required";
+    } else if (!emailRegex.test(formValues.user_email)) {
+      errors.user_email = "Email is not valid";
+    }
+
+    if (!formValues.message.trim()) {
+      errors.message = "Message is required";
+    }
+
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (validate()) {
+      emailjs
+        .sendForm("service_nlvjld9", "template_l7jjvqj", form.current, {
+          publicKey: "qxtBsAqACVCOH2sxs",
+        })
+        .then(
+          () => {
+            console.log("SUCCESS!");
+            e.target.reset();
+            setFormValues({
+              user_name: "",
+              user_lastName: "",
+              user_number: "",
+              user_password: "",
+              user_confirmPassword: "",
+              user_email: "",
+              message: "",
+            });
+            setFormErrors({});
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+    } else {
+      console.log("Validation failed");
+    }
+  };
+
+  return (
+    <div className="py-14 bg-gray-200">
+      <div className="container xl:max-w-[1164px] mx-auto px-4">
+        <h1 className="text-black xl:text-6xl sm:text-4xl text-2xl font-bold text-center mb-8">
+          Form
+        </h1>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="pt-12 flex flex-col gap-6"
+          action=""
+        >
+          <div className="flex flex-col gap-3">
+            <label className="text-black font-semibold text-4xl" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="border-[2px] border-solid border-gray-500 outline-none text-gray-500 text-2xl font-medium w-full rounded-[8px] p-[10px]"
+              name="user_name"
+              type="text"
+              value={formValues.user_name}
+              onChange={handleChange}
+              required
+            />
+            {formErrors.user_name && (
+              <p className="text-red-500 font-medium text-sm">
+                {formErrors.user_name}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col gap-3">
+            <label
+              className="text-black font-semibold text-4xl"
+              htmlFor="lastName"
+            >
+              Last Name
+            </label>
+            <input
+              className="border-[2px] border-solid border-gray-500 outline-none text-gray-500 text-2xl font-medium w-full rounded-[8px] p-[10px]"
+              name="user_lastName"
+              value={formValues.user_lastName}
+              onChange={handleChange}
+              type="text"
+            />
+            {formErrors.user_lastName && (
+              <p className="text-red-500 font-medium text-sm">
+                {formErrors.user_lastName}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col gap-3">
+            <label
+              className="text-black font-semibold text-4xl"
+              htmlFor="number"
+            >
+              Number
+            </label>
+            <input
+              className="border-[2px] border-solid border-gray-500 outline-none text-gray-500 text-2xl font-medium w-full rounded-[8px] p-[10px]"
+              name="user_number"
+              type="number"
+              value={formValues.user_number}
+              onChange={handleChange}
+              maxLength="10"
+              required
+            />
+            {formErrors.user_number && (
+              <p className="text-red-500 font-medium text-sm">
+                {formErrors.user_number}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col gap-3">
+            <label
+              className="text-black font-semibold text-4xl"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="border-[2px] border-solid border-gray-500 outline-none text-gray-500 text-2xl font-medium w-full rounded-[8px] p-[10px]"
+              name="user_password"
+              type="password"
+              value={formValues.user_password}
+              onChange={handleChange}
+              required
+            />
+            {formErrors.user_password && (
+              <p className="text-red-500 font-medium text-sm">
+                {formErrors.user_password}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col gap-3">
+            <label
+              className="text-black font-semibold text-4xl"
+              htmlFor="confirmPassword"
+            >
+              Confirm Password
+            </label>
+            <input
+              className="border-[2px] border-solid border-gray-500 outline-none text-gray-500 text-2xl font-medium w-full rounded-[8px] p-[10px]"
+              name="user_confirmPassword"
+              type="password"
+              value={formValues.user_confirmPassword}
+              onChange={handleChange}
+              required
+            />
+            {formErrors.user_confirmPassword && (
+              <p className="text-red-500 font-medium text-sm">
+                {formErrors.user_confirmPassword}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col gap-3">
+            <label
+              className="text-black font-semibold text-4xl"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="border-[2px] border-solid border-gray-500 outline-none text-gray-500 text-2xl font-medium w-full rounded-[8px] p-[10px]"
+              name="user_email"
+              type="email"
+              value={formValues.user_email}
+              onChange={handleChange}
+              required
+            />
+            {formErrors.user_email && (
+              <p className="text-red-500 font-medium text-sm">
+                {formErrors.user_email}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col gap-3">
+            <label
+              className="text-black font-semibold text-4xl"
+              htmlFor="message"
+            >
+              Message
+            </label>
+            <textarea
+              className="text-2xl font-medium bg-orange-100 border-[2px] border-solid border-gray-500 outline-none text-gray-500 w-full rounded-[8px] p-[10px]"
+              name="message"
+              value={formValues.message}
+              onChange={handleChange}
+              required
+            />
+            {formErrors.message && (
+              <p className="text-red-500 font-medium text-sm">
+                {formErrors.message}
+              </p>
+            )}
+          </div>
+          <input
+            type="submit"
+            value="Submit"
+            className="border border-solid mx-auto max-w-[150px] text-white text-[25px] font-bold bg-lime-950 border-lime-950 cursor-pointer py-[12px] px-[40px] rounded-[12px] mt-7"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FormEmailJsPractice;
